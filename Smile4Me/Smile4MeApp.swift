1807815c2fc149cd933702e0d70e41f52a4b28a9
Juan Carlos Pazos <datafox@outlook.com>
2025-03-20 13:20:17 -0600

    Initial Commit

---
+++ Smile4Me/Smile4MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smile4MeApp.swift
+//  Smile4Me
+//
+//  Created by Juan Carlos Pazos on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Smile4MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
